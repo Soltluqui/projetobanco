f0799c3508584b36dc0163fd3cd0cc892b6d126b
Solt <71570469+Soltluqui@users.noreply.github.com>
2023-10-16 15:38:14 -0400

    Update Atividade_banco.py

--- Atividade_banco.py
+++ Atividade_banco.py
@@ -1,49 +1,49 @@
-menu = "\n[d] Depositar \n[s] Sacar \n[e] Extrato \n[q] Sair\n"
-
 saldo = 0
 limite = 500
 extrato = ""
 numero_saques = 0
 LIMITE_SAQUES = 3
 
+
+menu = """
+[d] Depositar
+[s] Sacar
+[e] Extrato
+[q] Sair
+"""
+
+
 while True:
-    opcao = input(menu)
+    print(menu)
+    option = input("Insira a opção ")
 
-    if opcao == "d":
-        valor = float(input("Informe o valor para depósito: "))
-        if valor > 0:
-            saldo += valor
-            extrato += f"Deposito: R$ {valor:.2f}\n"
-
+    if option == "d":
+        deposito = float(input("Insira o valor para ser depositado: "))
+        if deposito > 0:
+            saldo += deposito
+            extrato += f"Deposito de {deposito:.2f} \n"
         else:
-            print("Valor inválido, tente novamente")
-        
-    elif opcao == "s":
-        valor = float(input("Insira o valor para saque "))
-        excedeu_saldo = valor > saldo
-        excedeu_limite = valor > limite
-        excedeu_saques = numero_saques >= LIMITE_SAQUES
+            print("Insira um valor válido")
 
-        if excedeu_saldo:
-            print("Sem saldo na conta, tente novamente")
-        elif excedeu_limite:
-            print("Excedeu o Limite de saque, tente novamente ")
-        elif excedeu_saques:
-            print("Numero maximo de saques atingido, aguarde até amanha")
-        elif valor > 0:
-            saldo -= valor
-            extrato += f"Saque: R$ {valor:.2f}\n"
-            numero_saques += 1
+    elif option == "s":
+        if LIMITE_SAQUES > 0 and saldo > 0:
+            sacar = float(input("Insira o valor para ser sacado: "))
+            resto = saldo - sacar
+            if sacar <= 0 or resto < 0 or sacar >= limite:
+                print("Insira um valor válido")
+            else:
+                saldo -= sacar
+                LIMITE_SAQUES -= 1
+                extrato += f"Saque no valor de {sacar:.2f}\n"
         else:
-            print("Valor invalido, tente novamente")
+            print("Você não pode sacar no momento")
 
-    elif opcao == "e":
-        print("\n Extrato da conta")
-        print("A conta não foi movimentada" if not extrato else extrato)
-        print(f"\n Saldo: R$ {saldo:.2f}")
-    
-    elif opcao == "q":
+    elif option == "e":
+        print("Seu extrato atual:")
+        print(f"O saldo atual na conta é de {saldo}")
+        print(f"Você ainda pode fazer {LIMITE_SAQUES} saques hoje")
+        print(extrato)
+    elif option == "q":
         break
-
     else:
-        print("Opção invalida, tente novamente")
+        print("Insira uma opção valida")
